2c852dab9d43a3e6f2962e85bf2f1c9d285ffc4f
CleidsonGoes <jorgegoes4@gmail.com>
2024-04-27 16:39:18 -0300

    req06 feito, aplicando o GoogleTranslator

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -1,3 +1,4 @@
+from deep_translator import GoogleTranslator
 from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
 
@@ -28,11 +29,15 @@ def translate_text():
     text_to_translate = request.form["text-to-translate"]
     translate_from = request.form["translate-from"]
     translate_to = request.form["translate-to"]
-    translated = 
+    translated_text = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
 
-    return render_template("index.html",
-                           languages=languages,
-                           text_to_translate=text_to_translate,
-                           translate_from=translate_from,
-                           translate_to=translate_to,
-                           translated=translated)
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated_text,
+    )
