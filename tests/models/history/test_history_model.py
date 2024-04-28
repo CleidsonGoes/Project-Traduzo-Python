430e296714b36c27082773314ca7821a470e7a23
CleidsonGoes <jorgegoes4@gmail.com>
2024-04-28 13:15:18 -0300

    teste da classe HistoryModel feito

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,18 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_list = HistoryModel.list_as_json()
+    data_list = json.loads(history_list)
+
+    for data in data_list:
+        assert "text_to_translate" in data
+        assert "translate_from" in data
+        assert "translate_to" in data
+        assert not data["text_to_translate"].islower()
+
+    assert data_list[1]["text_to_translate"] == "Do you love music?"
+    assert data_list[1]["translate_from"] == "en"
+    assert data_list[1]["translate_to"] == "pt"
